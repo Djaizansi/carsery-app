fa028b45dcd2c7068b96f9b6f37986ee9af69e7c
Djaizansi <youcef.jallali@gmail.com>
2021-11-25 01:37:07 +0100

    [AWS]: Aws configuration

--- aws/src/Controller/UploadController.php
+++ aws/src/Controller/UploadController.php
@@ -3,15 +3,24 @@
 namespace App\Controller;
 
 use App\Entity\Document;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class UploadController {
 
-    public function __invoke(Request $request){
+    public function __construct(private EntityManagerInterface $em){}
+    public function __invoke(Request $request)
+    {
+        $uploadedFile = $request->files->get('file');
+        if (!$uploadedFile) {
+            throw new BadRequestHttpException('"file" is required');
+        }
+
         $document = new Document();
-        $file = $request->files->get('file');
-        $document->setFile($file);
-        return new JsonResponse('Image enregistrée',201);
+        $document->file = $uploadedFile;
+
+        return $document;
     }
 }
--- aws/src/Entity/Document.php
+++ aws/src/Entity/Document.php
@@ -46,9 +46,9 @@ class Document
 
     /**
      * @var File
-     * @Vich\UploadableField(mapping="document", fileNameProperty="documentFileName")
+     * @Vich\UploadableField(mapping="aws", fileNameProperty="filePath")
      */
-    private $file;
+    public $file;
 
     /**
      * @var \DateTime
@@ -119,6 +119,14 @@ class Document
     public function setFile(File $file): void
     {
         $this->file = $file;
+
+        // VERY IMPORTANT:
+        // It is required that at least one field changes if you are using Doctrine,
+        // otherwise the event listeners won't be called and the file is lost
+        if ($file) {
+            // if 'updatedAt' is not defined in your entity, use another property
+            $this->updatedAt = new \DateTime('now');
+        }
     }
 }
 
