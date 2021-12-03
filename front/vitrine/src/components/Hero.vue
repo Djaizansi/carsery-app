811d4620c86ace2ae145f7d7141bc3a0eb5505cc
Marwane BOUABDELLAH <manbou92@hotmail.fr>
2021-12-03 10:59:03 +0100

    Add Hero Bulma + typervue

---
+++ front/vitrine/src/components/Hero.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="hero is-medium is-link">
+    <div class="hero-body">
+      <p class="title">
+        Le système de location de voiture repensé pour <vue-typer :text="string"></vue-typer>
+      </p>
+      <p class="subtitle">
+        lorem
+      </p>
+    </div>
+  </section>
+</template>
+
+<script>
+module.exports = {
+  data: function() {
+    return {
+      string: ['les particuliers', 'les professionnels', 'les novices', 'les passionnés']
+    }
+  }
+}
+</script>
