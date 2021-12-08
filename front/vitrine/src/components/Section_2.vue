72b3c60da6c9fd5a923beae6e97edc6ffe460898
Marwane BOUABDELLAH <manbou92@hotmail.fr>
2021-12-08 10:04:59 +0100

    Add second section to the vue

---
+++ front/vitrine/src/components/Section_2.vue
@@ -0,0 +1,45 @@
+<template>
+  <section class="hero is-white is-fullheight">
+    <div class="hero-body" data-aos="zoom-in-left">
+      <div class="container">
+        <div class="columns  is-vcentered reverse-columns">
+          <div data-aos="fade-right" class="column
+          is-10-mobile is-offset-1-mobile
+          is-10-tablet is-offset-1-tablet
+          is-5-desktop is-offset-1-desktop
+          is-5-widescreen is-offset-1-widescreen
+          is-5-fullhd is-offset-1-fullhd">
+            <h1 class="title titled is-2 mb-6">
+              Pour tout le monde, même Djaizanbite &#128513;
+            </h1>
+            <h2 class="subtitle subtitled m-1">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum cupiditate dolorum vitae dolores
+              nesciunt totam magni quas.
+            </h2>
+          </div>
+          <div data-aos="fade-down" class="column
+          is-10-mobile is-offset-1-mobile
+          is-10-tablet is-offset-1-tablet
+          is-4-desktop is-offset-1-desktop
+          is-4-widescreen is-offset-1-widescreen
+          is-4-fullhd is-offset-1-fullhd">
+            <figure class="image is-square">
+              <img src="https://www.svgrepo.com/show/138892/users.svg">
+            </figure>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Section_2"
+}
+</script>
+
+<style scoped>
+
+</style>
