8deb112bccb702ca6c6d283c8cdae426ff73a4da
Marwane BOUABDELLAH <manbou92@hotmail.fr>
2021-12-08 10:04:27 +0100

    Add first section

---
+++ front/vitrine/src/components/Section.vue
@@ -0,0 +1,46 @@
+<template>
+  <section class="hero is-white is-fullheight">
+    <div class="hero-body" data-aos="zoom-in-down">
+      <div class="container">
+        <div class="columns  is-vcentered">
+          <div data-aos="fade-left" class="column
+          is-10-mobile is-offset-1-mobile
+          is-10-tablet is-offset-1-tablet
+          is-4-desktop is-offset-1-desktop
+          is-4-widescreen is-offset-1-widescreen
+          is-4-fullhd is-offset-1-fullhd">
+            <figure class="image is-square">
+              <img src="https://www.svgrepo.com/show/24109/car.svg">
+            </figure>
+          </div>
+          <div data-aos="fade-down" class="column
+          is-10-mobile is-offset-1-mobile
+          is-10-tablet is-offset-1-tablet
+          is-5-desktop is-offset-1-desktop
+          is-5-widescreen is-offset-1-widescreen
+          is-5-fullhd is-offset-1-fullhd">
+            <h1 class="titled title is-2 mb-6">
+              Un tout nouveau système &#128175;
+            </h1>
+            <h2 class="subtitled subtitle m-1">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum cupiditate dolorum vitae dolores
+              nesciunt totam magni quas. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum cupiditate dolorum vitae dolores
+              nesciunt totam magni quas.
+            </h2>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Section"
+}
+</script>
+
+<style scoped>
+
+</style>
