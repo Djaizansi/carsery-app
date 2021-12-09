b190ce571e35a4f566af34642bbe9394063b9b11
Marwane BOUABDELLAH <manbou92@hotmail.fr>
2021-12-09 15:05:50 +0100

    add footer component

---
+++ front/vitrine/src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p class="has-text-white">
+        <strong class="has-text-white">CARSERY</strong> by <a href="https://jgthms.com">MBLH & DZTN</a>. The source code is licensed
+        <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
+        is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+      </p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+}
+</script>
+
+<style scoped>
+.footer {
+  background: #7957d5;
+}
+</style>
