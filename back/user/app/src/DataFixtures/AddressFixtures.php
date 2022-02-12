12ccfe85d8906e5a9b76d724cba48622ff4e74c0
Marwane BOUABDELLAH <manbou92@hotmail.fr>
2022-02-12 18:20:47 +0100

    [FIXTURE] Add reference for adresse

--- back/user/app/src/DataFixtures/AddressFixtures.php
+++ back/user/app/src/DataFixtures/AddressFixtures.php
@@ -8,13 +8,13 @@ use Doctrine\Persistence\ObjectManager;
 
 class AddressFixtures extends Fixture
 {
-
     public function load(ObjectManager $manager)
     {
         // TODO: Implement load() method.
         $city = ['Gennevilliers', 'Paris', 'Aubervilliers'];
         $street = ['2 rue Croix des vignes', '15 Aller de Nation', "34 rue de Jeanne d'Arc"];
         $code = ['92230', '75012', '93239'];
+
 
         for ($i=0;$i<3;$i++) {
             $address = (new Address())
@@ -25,6 +25,7 @@ class AddressFixtures extends Fixture
                 ->setRegion('Ile-De-France');
 
             $manager->persist($address);
+            $this->addReference($i, $address);
         }
         $manager->flush();
     }
